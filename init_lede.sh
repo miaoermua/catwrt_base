45def279c35ff6ed7c22609269c18718509e6a1f
喵二 <66155308+miaoermua@users.noreply.github.com>
2024-06-23 21:52:30 +0800

    Create init_lede.sh

---
+++ init_lede.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HOME_DIR="/home/$USER"
+LEDE_DIR="$HOME_DIR/lede"
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "请切换到非 root 用户进行编译。"
+    exit 1
+fi
+
+if [ ! -d "$LEDE_DIR" ]; then
+    echo "Cloning lede repository to $LEDE_DIR"
+    git clone https://github.com/coolsnowwolf/lede "$LEDE_DIR"
+fi
+
+cd "$LEDE_DIR" || exit
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+echo "请选择你的机型保存开始下一步准备环境!"
+make menuconfig
