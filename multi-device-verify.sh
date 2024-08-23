02e2be435e6a4ee5a0599637d6646457a6d848dc
喵二 <66155308+miaoermua@users.noreply.github.com>
2024-08-23 19:03:09 +0800

    Update multi-device-verify.sh

--- multi-device-verify.sh
+++ multi-device-verify.sh
@@ -20,12 +20,14 @@ initial_dir_hash=$(get_dir_hash)
 initial_config_hash=$(get_config_hash)
 initial_kernel_version=$(get_kernel_version)
 
+echo
+echo
 echo "初始目录 hash 值: $initial_dir_hash"
-echo "初始 .config 文件 hash 值: $initial_config_hash"
 echo "初始 kernel 版本: $initial_kernel_version"
+echo "初始 .config 文件 hash 值: $initial_config_hash"
 
 while true; do
-  read -p "multi-device-verify: 按 [1] 继续检查，按 [2] 退出并保留结果无误: " user_input
+  read -p "multi-device-verify: 按 [1] 继续检查，按 [2] 退出: " user_input
   if [ "$user_input" == "2" ]; then
     echo "multi-device-verify: 脚本结束"
     exit 0
@@ -37,22 +39,11 @@ while true; do
 done
 
 current_config_hash=$(get_config_hash)
-
-if [ "$initial_config_hash" == "$current_config_hash" ]; then
-  echo "multi-device-verify: .config 文件的 hash 无变化，无法继续校验，保留初始结果"
-  exit 1
-fi
-
 current_dir_hash=$(get_dir_hash)
 current_kernel_version=$(get_kernel_version)
 
-if [ "$initial_dir_hash" != "$current_dir_hash" ]; then
-  echo "multi-device-verify: 目录 hash 值发生变化，脚本结束"
-  exit 1
-fi
-
-if [ "$initial_kernel_version" != "$current_kernel_version" ]; then
-  echo "multi-device-verify: Packages 文件中的 kernel 版本发生变化，脚本结束"
+if [ "$initial_config_hash" != "$current_config_hash" ] || [ "$initial_dir_hash" != "$current_dir_hash" ] || [ "$initial_kernel_version" != "$current_kernel_version" ]; then
+  echo "multi-device-verify: 目录 hash 值或 kernel 版本发生变化，脚本结束"
   exit 1
 fi
 
